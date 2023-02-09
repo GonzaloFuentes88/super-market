45823e9159e3cadda62edf7462bba681041a45f3
Gonzalo <fuentesgonzalo88@gmail.com>
2023-02-08 22:41:09 -0300

    Modificamos la contraseña del user dentro de import.sql

--- spring-boot-supermarket/src/main/resources/import.sql
+++ spring-boot-supermarket/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO users (username, password) VALUES ('Gonzalo','123');
+INSERT INTO users (username, password) VALUES ('Gonzalo','123456');
 INSERT INTO carnes (proveedor,nombre,peso,precio,fecha_elaboracion,fecha_vencimiento,animal,categoria) VALUES ('Pechuga King-Chicken','Pechuga de Pollo',1000,800,NOw(),NOW(), 'Gallina','Economica');
 INSERT INTO verduras (proveedor,nombre,peso,precio,fecha_elaboracion,fecha_vencimiento,categoria,congelado) VALUES ('Verduras Pompeya','Tomate',1000,450,NOw(),NOW(), 'Premium',TRUE);
