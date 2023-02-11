131ce9b8e8651d34bf977b9446048e0d228be59f
Gonzalo <fuentesgonzalo88@gmail.com>
2023-02-11 14:12:19 -0300

    Implementacion de la clase service IAnnualPercentagesImpl

---
+++ spring-boot-supermarket/src/main/java/com/bolsadeideas/springboot/app/service/ServiceAnnualPercentagesImpl.java
@@ -0,0 +1,41 @@
+package com.bolsadeideas.springboot.app.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.dao.IAnnualPercentagesDao;
+import com.bolsadeideas.springboot.app.entity.AnnualPercentages;
+
+
+@Service
+public class ServiceAnnualPercentagesImpl implements IServiceAnnualPercentages {
+	
+	@Autowired
+	IAnnualPercentagesDao annualPercentagesDao;
+	
+	@Override
+	public AnnualPercentages findByYearAndCategoria(Long year, String categoria) {
+		return annualPercentagesDao.findByYearAndCategoria(year, categoria).orElse(null);
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<AnnualPercentages> findAll() {
+		return annualPercentagesDao.findAll();
+	}
+	
+	
+	@Override
+	public void deleteByYearAndCategoria(Long year, String categoria) {
+		annualPercentagesDao.deleteByYearAndCategoria(year,categoria);
+	}
+	@Override
+	public void save(AnnualPercentages annualPercentages) {
+		annualPercentagesDao.save(annualPercentages);
+	}
+	
+}
