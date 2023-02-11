8379a2458692f0dc016c2356609dafeaab064834
Gonzalo <fuentesgonzalo88@gmail.com>
2023-02-11 14:17:07 -0300

    Agregamos metodos al IAnnuaPercentagesDao.java

---
+++ spring-boot-supermarket/src/main/java/com/bolsadeideas/springboot/app/dao/IAnnualPercentagesDao.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bolsadeideas.springboot.app.entity.AnnualPercentages;
+
+public interface IAnnualPercentagesDao extends JpaRepository<AnnualPercentages, Long>{
+
+	public Optional<AnnualPercentages> findByYearAndCategoria(Long year, String categoria);
+	public void deleteByYearAndCategoria(Long year, String categoria);
+}
